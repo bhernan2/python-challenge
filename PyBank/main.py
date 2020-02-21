ace53afd62b7f2003fe0fc80e77c87cb6749463b
Bianca A. Hernandez <bhernan2@gmail.com>
2020-02-20 19:14:13 -0600

    total_months found

--- PyBank/main.py
+++ PyBank/main.py
@@ -27,3 +27,50 @@ with open("budget_data.csv", newline = '') as file:
     reader = csv.reader(file, delimiter =",")
     for row in reader:
         print(row)
+# 2/18/2020
+# Your task is to create a Python script that analyzes the records to calculate each of the following:
+
+# The total number of months included in the dataset
+
+# The net total amount of "Profit/Losses" over the entire period
+
+# The average of the changes in "Profit/Losses" over the entire period
+
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in losses (date and amount) over the entire period
+
+
+#libraries needed
+import os
+import csv
+
+# file location
+budget_df=os.path.join("..", "PyBank", "budget_data.csv")
+budget_df
+
+# create empty list for total months 
+total_months = []
+# open csv in reader mode 
+with open("budget_data.csv", newline ='') as budeget_file:
+    #store budget_df data in cvsreader 
+    csvreader = csv.reader(budget_df, delimiter = ",")
+    #skip column labels
+    header = next(csvreader)
+    #iterate through monthly data
+    for row in csvreader:
+        print(f"Total Months: {len(total_months)}")
+# output incorrect 
+# Total Months: 0
+# open csv in reader mode 
+
+with open("budget_data.csv", "r") as csvfile:
+    #store budget_df data in csvreader 
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    #skip column labels
+    next(csvreader, None)
+    #iterate through monthly data
+    for months in csvreader:
+        total_months += 1
+    print(f"Total Months: {str(total_months)}")
+#Total Months: 86 correct!
